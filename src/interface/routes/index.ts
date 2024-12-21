a9e48eb3923bb6d860730dd44ce63a44e29236b0
Jay200123 <kenken.ayonon@gmail.com>
2024-12-21 13:16:22 +0800

    add route interface and middleware types for express integration

---
+++ src/interface/routes/index.ts
@@ -0,0 +1,35 @@
+import express, { Router, Request, Response, NextFunction } from "express";
+import { AuthenticatedRequest } from "../middleware";
+
+type Middleware = (req: Request, res: Response, next: NextFunction) => void;
+
+type AuthMiddleware = (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => void;
+
+type Route = {
+  method: keyof Router;
+  path: string;
+  middleware?: AuthMiddleware[];
+  role?: string[];
+  handler: Middleware;
+};
+
+type ErrorMiddleware = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => void;
+
+export {
+  express,
+  Route,
+  Router,
+  Request,
+  Response,
+  NextFunction,
+  ErrorMiddleware,
+};
