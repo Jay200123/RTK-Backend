4be84a8c1b43d46c2cee0fff0ae46d68d56c220d
Jay200123 <kenken.ayonon@gmail.com>
2024-12-23 00:16:50 +0800

    Add order route export to main routes index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,3 +2,4 @@ export { default as user } from "./user/route";
 export { default as authentication } from "./authentication/route";
 export { default as brand } from "./brand/route";
 export { default as product } from "./product/route";
+export { default as order } from "./order/route";
