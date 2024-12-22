bb4187be3318d90d5b50887d7669b9e1cadfbd87
Jay200123 <kenken.ayonon@gmail.com>
2024-12-22 11:19:57 +0800

    Implement user management controller with CRUD operations and image handling

---
+++ src/routes/user/controller.ts
@@ -0,0 +1,67 @@
+import { Request, Response, NextFunction, Image } from "../../interface";
+import { ErrorHandler, SuccessHandler } from "../../utils";
+import { UserService } from "./service";
+import { STATUSCODE } from "../../constants";
+import { uploadImage } from "../../utils";
+import { cloudinary } from "../../config";
+
+export class UserController {
+  static async getAllUsers(req: Request, res: Response, next: NextFunction) {
+    const data = await UserService.getAll();
+    return !data || data?.length === STATUSCODE.ZERO
+      ? SuccessHandler(res, "User records found", data)
+      : new ErrorHandler("No user records found");
+  }
+
+  static async getOneUser(req: Request, res: Response, next: NextFunction) {
+    const data = await UserService.getOne(req.params.id);
+    return !data
+      ? new ErrorHandler("User not found")
+      : SuccessHandler(res, "User found", data);
+  }
+
+  static async AddUser(req: Request, res: Response, next: NextFunction) {
+    const image = await uploadImage(req.files as Express.Multer.File[], []);
+    const data = await UserService.Add({
+      ...req.body,
+      image: image,
+    });
+    return SuccessHandler(res, "User created", data);
+  }
+
+  static async updateUser(req: Request, res: Response, next: NextFunction) {
+    const user = await UserService.getOne(req.params.id);
+
+    const oldImage = Array?.isArray(user?.image)
+      ? user?.image?.map((u) => u?.public_id)
+      : [];
+
+    let image: Image[];
+
+    image = await uploadImage(req.files as Express.Multer.File[], oldImage);
+
+    const data = await UserService.updateById(req.params.id, {
+      ...req.body,
+      image: image,
+    });
+
+    return SuccessHandler(res, "User record updated", data);
+  }
+  static async deleteUser(req: Request, res: Response, next: NextFunction) {
+    const user = await UserService.getOne(req.params.id);
+
+    const userImage = Array.isArray(user?.image)
+      ? user.image.map((i) => i?.public_id)
+      : [];
+
+    if (userImage.length > 0) {
+      await cloudinary.api.delete_resources(userImage);
+    }
+
+    const data = await UserService.deleteById(req.params.id);
+
+    return !data
+      ? next(new ErrorHandler("No user record found"))
+      : SuccessHandler(res, "user deleted successfully", data);
+  }
+}
