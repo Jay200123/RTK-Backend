9610058cdf2e67399115e54f613545bc1e2943f2
Jay200123 <kenken.ayonon@gmail.com>
2024-12-22 11:19:17 +0800

    Uncomment and enable token generation functionality in middleware

--- src/middleware/generateToken.ts
+++ src/middleware/generateToken.ts
@@ -1,15 +1,15 @@
-// import { jwt, SignOptions, TokenPayload } from "../interface";
+import { jwt, SignOptions, TokenPayload } from "../interface";
 
-// export const generateToken = (
-//     payload: TokenPayload = {} as TokenPayload,
-//     expiresIn: string = '1d'
-// ): string => {
-//     const options: SignOptions = { expiresIn };
+export const generateToken = (
+    payload: TokenPayload = {} as TokenPayload,
+    expiresIn: string = '1d'
+): string => {
+    const options: SignOptions = { expiresIn };
 
-//     const TokenPayload = {
-//         ...payload,
-//         _id: payload._id.toString(),
-//     }
+    const TokenPayload = {
+        ...payload,
+        _id: payload._id.toString(),
+    }
 
-//     return jwt.sign(TokenPayload, process.env.JWT_SECRET, options);
-// }
+    return jwt.sign(TokenPayload, process.env.JWT_SECRET, options);
+}
