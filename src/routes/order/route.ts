78ed44d9b8ee4d93c9ca147e0527546dea6ac1dd
Jay200123 <kenken.ayonon@gmail.com>
2024-12-23 00:17:07 +0800

    Add order routes with CRUD operations and middleware for authentication

---
+++ src/routes/order/route.ts
@@ -0,0 +1,51 @@
+import { Route, Router } from "../../interface";
+import { OrderController } from "./controller";
+import { verifyToken, userRole } from "../../middleware";
+import { METHOD, PATH, ROLE } from "../../constants";
+
+const router = Router();
+
+const orderRoutes: Route[] = [
+  {
+    method: METHOD.GET as keyof Router,
+    path: PATH.ORDERS,
+    role: [ROLE.ADMIN],
+    middleware: [verifyToken],
+    handler: OrderController.getAllOrders,
+  },
+  {
+    method: METHOD.GET as keyof Router,
+    path: PATH.ORDER_ID,
+    role: [ROLE.ADMIN],
+    middleware: [verifyToken],
+    handler: OrderController.getOneOrder,
+  },
+  {
+    method: METHOD.POST as keyof Router,
+    path: PATH.ORDERS,
+    role: [ROLE.ADMIN],
+    middleware: [verifyToken],
+    handler: OrderController.addOrder,
+  },
+  {
+    method: METHOD.PATCH as keyof Router,
+    path: PATH.EDIT_ORDER_ID,
+    role: [ROLE.ADMIN],
+    middleware: [verifyToken],
+    handler: OrderController.updateOrder,
+  },
+  {
+    method: METHOD.DELETE as keyof Router,
+    path: PATH.ORDER_ID,
+    role: [ROLE.ADMIN],
+    middleware: [verifyToken],
+    handler: OrderController.deleteOrder,
+  },
+];
+
+orderRoutes.forEach((route) => {
+  const { method, path, handler, role = [], middleware = [] } = route;
+  router[method as any](path, middleware.concat(userRole(...role)), handler);
+});
+
+export default router;
