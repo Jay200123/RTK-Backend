3118e85c87f5a8b4d7521cb83e55d55a5cf62d7e
Jay200123 <kenken.ayonon@gmail.com>
2024-12-22 15:49:08 +0800

    Add brand route export to main routes index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
 export { default as user } from "./user/route";
 export { default as authentication } from "./authentication/route";
+export { default as brand } from "./brand/route";
 export { default as product } from "./product/route";
