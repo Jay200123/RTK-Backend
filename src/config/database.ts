dba3fe9a0695fd51c19b4d5bb18acd92044f978d
Jay200123 <kenken.ayonon@gmail.com>
2024-12-22 11:18:34 +0800

    Improve database connection error handling and logging

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,10 +2,12 @@ import { mongoose } from "../interface";
 import { STATUSCODE } from "../constants";
 
 export const connectDB = async () => {
-    try {
-        mongoose.set("strictQuery", false);
-        await mongoose.connect(process.env.DATABASE_URI);
-    } catch (err) {
-        process.exit(STATUSCODE.ONE);
-    }
-}
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.DATABASE_URI);
+  } catch (err) {
+    console.log(err);
+    console.log("Error database connection cannot be established");   
+    process.exit(STATUSCODE.ONE);
+  }
+};
