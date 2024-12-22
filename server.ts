3e5e47717fd7589720acfcfc881d66a2f88f3efa
Jay200123 <kenken.ayonon@gmail.com>
2024-12-22 11:18:28 +0800

    Set up Express server with database connection and middleware

--- server.ts
+++ server.ts
@@ -1,1 +1,39 @@
-console.log("Hi there!");
+import { connectDB, globalEnvironment } from "./src/config";
+import { express, mongoose } from "./src/interface";
+import { upload } from "./src/utils";
+import { user, authentication } from "./src/routes";
+import { errorJson, errorHandler } from "./src/middleware";
+
+connectDB();
+globalEnvironment();
+
+const app = express();
+
+app.use(upload.array("image"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  const data = { message: "Express server is running" };
+  res.json(data);
+});
+
+app.use("/api/v1", authentication, user);
+
+app.get("*", (req, res) => {
+  const data = { message: "Route not found" };
+  res.json(data);
+});
+
+app.use(errorJson);
+app.use(errorHandler);
+
+mongoose.connection.once("open", () => {
+  app.listen(process.env.PORT);
+  console.log(`Mongoose Database connected`);
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  process.exit(1);
+});
