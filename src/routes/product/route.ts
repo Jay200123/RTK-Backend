9ef03f4dd3d6d2bdf532dcdef31a340d5e9ea12b
Jay200123 <kenken.ayonon@gmail.com>
2024-12-30 19:42:41 +0800

    Remove role and middleware checks from product routes for simplified access control

--- src/routes/product/route.ts
+++ src/routes/product/route.ts
@@ -9,43 +9,33 @@ const userRoutes: Route[] = [
   {
     method: METHOD.GET as keyof Router,
     path: PATH.PRODUCTS,
-    role: [ROLE.ADMIN, ROLE.USER],
-    middleware: [verifyToken],
     handler: ProductController.getAllProducts,
   },
   {
     method: METHOD.GET as keyof Router,
     path: PATH.PRODUCT_ID,
-    role: [ROLE.ADMIN, ROLE.USER],
-    middleware: [verifyToken],
     handler: ProductController.getOneProduct,
   },
   {
     method: METHOD.POST as keyof Router,
     path: PATH.PRODUCTS,
-    role: [ROLE.ADMIN],
-    middleware: [verifyToken],
     handler: ProductController.AddProduct,
   },
   {
     method: METHOD.PATCH as keyof Router,
     path: PATH.EDIT_PRODUCT_ID,
-    role: [ROLE.ADMIN],
-    middleware: [verifyToken],
     handler: ProductController.updateProduct,
   },
   {
     method: METHOD.DELETE as keyof Router,
     path: PATH.PRODUCT_ID,
-    role: [ROLE.ADMIN],
-    middleware: [verifyToken],
     handler: ProductController.deleteProduct,
   },
 ];
 
 userRoutes.forEach((route) => {
-  const { method, path, role = [], middleware = [], handler } = route;
-  router[method as any](path, middleware.concat(userRole(...role)), handler);
+  const { method, path, handler } = route;
+  router[method as any](path, handler);
 });
 
 export default router;
