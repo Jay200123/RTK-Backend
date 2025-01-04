5ab75343b9db3fe527cfc777542a84517de99213
Jay200123 <kenken.ayonon@gmail.com>
2025-01-04 16:15:33 +0800

    Add error handling for undefined DATABASE_URI in database connection

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,7 +4,11 @@ import { STATUSCODE } from "../constants";
 export const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    await mongoose.connect(process.env.DATABASE_URI);
+    const dbUri = process.env.DATABASE_URI;
+    if (!dbUri) {
+      throw new Error("DATABASE_URI is not defined");
+    }
+    await mongoose.connect(dbUri);
   } catch (err) {
     console.log(err);
     console.log("Error database connection cannot be established");   
