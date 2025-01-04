75a0a3374e7edde656fb4438bdbdad8223c9d606
Jay200123 <kenken.ayonon@gmail.com>
2025-01-04 16:41:50 +0800

    Fix allowed origins by removing trailing slashes

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://e-com-backend-peach.vercel.app/",
-    "https://e-com-front-end.vercel.app/"
+    "https://e-com-backend-peach.vercel.app",
+    "https://e-com-front-end.vercel.app"
 ]
 
 export default allowedOrigins;
