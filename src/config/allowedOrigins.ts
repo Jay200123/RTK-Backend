40cbcc97db24256b376edcf5794acee88664fb29
Jay200123 <kenken.ayonon@gmail.com>
2025-01-18 21:57:13 +0800

    Fix allowed origins by removing trailing slashes

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://rtk-backend-alpha.vercel.app/",
-    "https://e-com-redux-toolkit.vercel.app/"
+    "https://rtk-backend-alpha.vercel.app",
+    "https://e-com-redux-toolkit.vercel.app"
 ]
 
 export default allowedOrigins;
