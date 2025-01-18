cdd737e4a428fdf54ffe38c1395af28e17b7e785
Jay200123 <kenken.ayonon@gmail.com>
2025-01-18 19:27:46 +0800

    Add image field to Brand interface

--- src/interface/models/brand.ts
+++ src/interface/models/brand.ts
@@ -1,8 +1,10 @@
 import { mongoose } from "../mongoose";
+import { Image } from "../image";  
 
 interface Brand extends Document {
   _id: mongoose.Types.ObjectId;
   brand_name: string;
-}
+  image: Image[],
+}   
 
 export type { Brand };
