2824c07a81c92714b2a98a6084d49bd3f94c18ce
Jay200123 <kenken.ayonon@gmail.com>
2025-01-22 16:33:26 +0800

    Add rating route export to main routes index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,3 +3,4 @@ export { default as authentication } from "./authentication/route";
 export { default as brand } from "./brand/route";
 export { default as product } from "./product/route";
 export { default as order } from "./order/route";
+export { default as rating } from "./rating/route"; 
