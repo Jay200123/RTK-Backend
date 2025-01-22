11166f668b2716161733333b92d5de9c9f135f14
Jay200123 <kenken.ayonon@gmail.com>
2025-01-22 16:33:36 +0800

    Add rating model with schema definition for ratings

---
+++ src/routes/rating/model.ts
@@ -0,0 +1,31 @@
+import { Ratings, Schema, model, mongoose } from "../../interface";
+import { RESOURCE } from "../../constants";
+
+const ratingSchema: Schema<Ratings> = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: RESOURCE.USERS,
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: RESOURCE.PRODUCTS,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  image: [
+    {
+      public_id: String,
+      url: String,
+      originalname: String,
+    },
+  ],
+});
+
+const Rating = model<Ratings>(RESOURCE.RATINGS, ratingSchema);
+export default Rating;
