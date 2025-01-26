fdbdb7f47b48be18b06035c2f431762574a164c0
Jay200123 <kenken.ayonon@gmail.com>
2025-01-26 20:47:50 +0800

    Add order reference to Ratings model

--- src/interface/models/rating.ts
+++ src/interface/models/rating.ts
@@ -1,15 +1,17 @@
 import { Image } from "../image";
 import { Product } from "./product";
 import { User } from "./user";
+import { Order } from "./order";
 import mongoose from "mongoose";
 
 interface Ratings extends Document {
-    _id: string;
-    rating: number;
-    description: string;
-    product: mongoose.Schema.Types.ObjectId | Product;  
-    user: mongoose.Schema.Types.ObjectId | User;
-    image: Image[];   
+  _id: string;
+  rating: number;
+  description: string;
+  product: mongoose.Schema.Types.ObjectId | Product;
+  user: mongoose.Schema.Types.ObjectId | User;
+  order: mongoose.Schema.Types.ObjectId | Order;
+  image: Image[];
 }
 
 export type { Ratings };
