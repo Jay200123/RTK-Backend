8e9b14ba89f3b32c706de21ee6b8c8c5be6cfc7c
Jay200123 <kenken.ayonon@gmail.com>
2025-01-28 16:58:57 +0800

    Refactor BrandController to improve image handling in add and update methods

--- src/routes/brand/controller.ts
+++ src/routes/brand/controller.ts
@@ -3,13 +3,13 @@ import { ErrorHandler, SuccessHandler } from "../../utils";
 import { BrandService } from "./service";
 import { STATUSCODE } from "../../constants";
 import { uploadImage } from "../../utils";
-import { cloudinary } from "../../config";
+import { cloudinary } from "../../config";  
 
 export class BrandController {
   static async getAllBrands(req: Request, res: Response, next: NextFunction) {
     const data = await BrandService.getAll();
     return !data || data?.length === STATUSCODE.ZERO
-      ?next( new ErrorHandler("No brands records found"))
+      ? next(new ErrorHandler("No brands records found"))
       : SuccessHandler(res, "brands records found", data);
   }
 
@@ -21,7 +21,7 @@ export class BrandController {
   }
 
   static async AddBrand(req: Request, res: Response, next: NextFunction) {
-    const image = await uploadImage(req.files as Express.Multer.File[], []);   
+    const image = await uploadImage(req.files as Express.Multer.File[], []);
     const data = await BrandService.Add({
       ...req.body,
       image: image,
@@ -30,14 +30,38 @@ export class BrandController {
   }
 
   static async updateBrand(req: Request, res: Response, next: NextFunction) {
+    const brand = await BrandService.getById(req.params.id);
+
+    const oldImage = Array?.isArray(brand?.image)
+      ? brand?.image?.map((b) => b?.public_id)
+      : [];
+
+    let image: Image[];
+
+    if (Array.isArray(req.files) && req.files.length > 0) {
+      image = await uploadImage(req.files as Express.Multer.File[], oldImage);
+    } else {
+      image = brand && Array.isArray(brand.image) ? brand.image : [];
+    }
+
     const data = await BrandService.updateById(req.params.id, {
       ...req.body,
+      image: image,
     });
 
     return SuccessHandler(res, "brand record updated", data);
   }
 
   static async deleteBrand(req: Request, res: Response, next: NextFunction) {
+    const brand = await BrandService.getById(req.params.id);
+
+    const brandImage = Array?.isArray(brand?.image)
+      ? brand?.image?.map((b) => b?.public_id)
+      : [];
+    if (brandImage.length > 0) {
+      await cloudinary.api.delete_resources(brandImage);
+    }
+
     const data = await BrandService.deleteById(req.params.id);
 
     return !data
