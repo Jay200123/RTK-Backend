4547775626597c985068b5ab328eceeda5899186
Jay200123 <kenken.ayonon@gmail.com>
2025-02-08 18:12:20 +0800

    Refactor ProductService tests for improved readability and add a test for product creation

--- test/service/product.test.ts
+++ test/service/product.test.ts
@@ -53,7 +53,9 @@ describe("Product Service", () => {
       },
     ];
 
-    const mockPopulate = jest.fn<() => Promise<typeof data>>().mockResolvedValue(data);
+    const mockPopulate = jest
+      .fn<() => Promise<typeof data>>()
+      .mockResolvedValue(data);
 
     (Product.find as jest.Mock).mockReturnValue({
       populate: mockPopulate,
@@ -63,7 +65,10 @@ describe("Product Service", () => {
     expect(res).toBeTruthy();
     expect(res).toEqual(data);
     expect(Product.find).toHaveBeenCalledTimes(1);
-    expect(mockPopulate).toHaveBeenCalledWith({ path: "brand", select: "brand_name" });
+    expect(mockPopulate).toHaveBeenCalledWith({
+      path: "brand",
+      select: "brand_name",
+    });
   });
   test("Get product by id", async () => {
     const data = {
@@ -83,22 +88,49 @@ describe("Product Service", () => {
       isNewlyCreated: true,
       image: [],
       __v: 0,
-    }
+    };
 
-    const mockPopulate = jest.fn<()=> Promise<typeof data>>().mockResolvedValue(data);
+    const mockPopulate = jest
+      .fn<() => Promise<typeof data>>()
+      .mockResolvedValue(data);
 
     (Product.findById as jest.Mock).mockReturnValue({
       populate: mockPopulate,
     });
 
     const res = await ProductService.getOne("60f8c4c4c4c4c4c4c4c4b3");
-    expect(res).toBeTruthy(); 
+    expect(res).toBeTruthy();
     expect(res).toEqual(data);
     expect(Product.findById).toHaveBeenCalledTimes(1);
-    expect(mockPopulate).toHaveBeenCalledWith({ path: "brand", select: "brand_name" }); 
+    expect(mockPopulate).toHaveBeenCalledWith({
+      path: "brand",
+      select: "brand_name",
+    });
   });
 
-//   test("Create product", async () => {});
-//   test("Update product by id", async () => {});
-//   test("Delete product by id", async () => {});
+  test("Create product", async () => {
+    const data = {
+      _id: "60f8c4c4c4c4c4c4c4c4b3",
+      brand: "60f8c4c4c4c4c4c4c4c4b3",
+      product_name: "Product 2",
+      price: 2000,
+      description: "Lorem Ipsum",
+      color: "#000000",
+      category: "Computer",
+      quantity: 10,
+      isNewlyCreated: true,
+      image: [],
+      __v: 0,
+    };
+
+    (
+      Product.create as jest.MockedFunction<typeof Product.create>
+    ).mockResolvedValue(data as any);
+    const res = await ProductService.Add(data as any);
+    expect(res).toBeTruthy();
+    expect(res).toEqual(data);
+  });
+
+  //   test("Update product by id", async () => {});
+  //   test("Delete product by id", async () => {});
 });
